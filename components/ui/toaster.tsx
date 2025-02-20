4b638638d1ca0d339e29767723046886b039d935
zeusgodyt <anurag.kumar6240@gmail.com>
2025-02-20 13:05:35 +0530

    Agian Fixed toast notification

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -15,21 +15,19 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
+      {toasts.map(({ id, title, description, action, ...props }) => (
+        <Toast key={id} {...props}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+          {action}
+          <ToastClose />
+        </Toast>
+      ))}
+
+      {/* ✅ Fixed: Toasts now appear at the top-center */}
+      <ToastViewport className="fixed top-5 left-1/2 transform -translate-x-1/2 w-auto max-w-md" />
     </ToastProvider>
   )
 }
